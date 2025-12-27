6991c8542c2f7c0aedbe9dd502ad62ec9d6e4832
VagishKapila <vaakapila@gmail.com>
2025-12-27 08:05:47 -0800

    Update project_files.py

--- backend/app/models/project_files.py
+++ backend/app/models/project_files.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from app.db import Base
 
-
 class ProjectFile(Base):
     __tablename__ = "project_files"
 
