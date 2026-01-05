dcb0beb59d949337d3d6668f432e305614d230d6
VagishKapila <vaakapila@gmail.com>
2026-01-04 18:28:56 -0800

    DB snapshot: vendor_calls + vendor_scores (known-good)

---
+++ schema_backup_2026_01_04.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict fj9eZr3b206geOEv37JBcHa2rQZal5qWXzZ6GCCdxb6lz2JFRifc6wOJdzKsF9N
+
+-- Dumped from database version 15.15 (Postgres.app)
+-- Dumped by pg_dump version 15.15 (Postgres.app)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict fj9eZr3b206geOEv37JBcHa2rQZal5qWXzZ6GCCdxb6lz2JFRifc6wOJdzKsF9N
+
